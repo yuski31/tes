c8db3ce8e4207cb9dcf20c64c4680fe9daf56d0c
yuski31 <shintstuff317@gmail.com>
2025-09-19 10:28:46 +0000

    docker img update #04

--- components/icons/MagicWandIcon.tsx
+++ components/icons/MagicWandIcon.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 
 export const MagicWandIcon: React.FC<{ className?: string }> = ({ className = "w-6 h-6" }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 11c.828 0 1.5.672 1.5 1.5S12.828 14 12 14s-1.5-.672-1.5-1.5.672-1.5 1.5-1.5z" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M2.5 13.5l3-3m5-5l3-3m-3 3l-3 3m3-3l3 3m-3-3V3m0 0v3m0 0H9m3 0h3m-3 0v3m0 0H9m3 0h3m-3-3l3-3m-3 3l-3 3m5 5l3 3m-3-3l-3-3m3 
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0m0 0a2.928 2.928 0 100-5.769 2.928 2.928 0 000 5.769zM10.5 19.5l-3 3m0 0l-3-3m3 3V17.5" />
+    </svg>
+);
